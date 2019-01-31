28519a25bd3c1ef1af091793d53d01b9835897a6
sean donnelly <seandonnelly@seans-MacBook-Pro.local>
2019-01-31 14:29:38 +0000

    remove unused code

--- securenote/api/Startup.cs
+++ securenote/api/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
-using Microsoft.AspNetCore.DataProtection;
 
 namespace api
 {
